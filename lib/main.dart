6cf17e10bc36e7e6d02886d5f18e61aea3d91af8
Lans12345 <olanalans12345@gmail.com>
2024-09-21 08:04:08 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:dog_classification_app/firebase_options.dart';
 import 'package:dog_classification_app/screens/auth/login_screen.dart';
 import 'package:dog_classification_app/screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: 'petskin-af728"',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
